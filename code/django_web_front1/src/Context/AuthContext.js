89bb79da478be00b9474ec2f6e9f237963d4075e
BranchDev110 <steven.fredericks.dev110@gmail.com>
2022-10-17 05:10:49 -0400

    Set up Context Provider

--- code/django_web_front1/src/Context/AuthContext.js
+++ code/django_web_front1/src/Context/AuthContext.js
@@ -3,3 +3,12 @@ import { createContext } from "react";
 const AuthContext = createContext();
 
 export default AuthContext;
+
+export const AuthProvider = ({children}) => {
+  const contextData = {
+
+  }
+  return (
+    <AuthContext.Provider value={contextData}>{children}</AuthContext.Provider>
+  )
+}
